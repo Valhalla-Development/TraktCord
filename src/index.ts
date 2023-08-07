64758d7a0c08edb40cbd54a05deaff8cafc16dd9
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-08-07 16:42:58 +0100

    Launch at login logic

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,34 @@
 import { platform } from 'node:process';
 import path from 'path';
 import {
-    app, Menu, nativeImage, Tray,
+    app, Menu, Tray,
 } from 'electron';
 
 let tray: Tray;
 
 app.whenReady().then(() => {
+    // Check if login launch item is enabled
+    function isLaunchAtLogin() {
+        const settings = app.getLoginItemSettings();
+        if (platform === 'darwin') {
+            return settings.openAtLogin;
+        } if (platform === 'win32') {
+            return settings.launchItems.some((item) => item.name === 'TraktCord');
+        }
+        return false;
+    }
+
+    // Login launch item
+    function toggleLaunchAtLogin() {
+        const settings = app.getLoginItemSettings();
+        app.setLoginItemSettings({ openAtLogin: !settings.openAtLogin });
+    }
+
     if (platform === 'darwin') {
         app.dock.hide();
     }
 
-    const assetsPath = app.isPackaged ? path.join(process.resourcesPath, "assets") : "assets";
+    const assetsPath = app.isPackaged ? path.join(process.resourcesPath, 'assets') : 'assets';
 
     tray = new Tray(`${assetsPath}/Icon.png`);
 
@@ -20,6 +37,9 @@ app.whenReady().then(() => {
     const contextMenu = Menu.buildFromTemplate([
         { label: 'placeholder', type: 'normal', click: () => console.log('placeholder clicked') },
         { type: 'separator' },
+        {
+            label: 'Launch at login', type: 'checkbox', checked: isLaunchAtLogin(), click: toggleLaunchAtLogin,
+        },
         { label: 'Quit', accelerator: quitAccelerator, click: () => app.quit() },
     ]);
 

2389dbb8f197a13aa52ff88f04bee93b6175a5f7
Ragnar Lothbrok <30740511+RagnarLothbrok-Odin@users.noreply.github.com>
2023-08-09 14:08:08 +0100

    Create `aboutItem`

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { app } from 'electron';
+import { app, dialog, NativeImage } from 'electron';
 
 // Check if login launch item is enabled
 export function isLaunchAtLogin(appInstance: typeof app, platform: string): boolean {
@@ -18,4 +18,17 @@ export function isLaunchAtLogin(appInstance: typeof app, platform: string): bool
 export function toggleLaunchAtLogin(appInstance: typeof app) {
     const settings = appInstance.getLoginItemSettings();
     appInstance.setLoginItemSettings({ openAtLogin: !settings.openAtLogin });
+}
+
+// About menu
+export async function aboutItem(trayIcon: NativeImage) {
+    const options = {
+        message: 'TraktCord',
+        type: 'info' as const,
+        title: 'About TraktCord',
+        detail: `Version 1.0.0\nCopyright ${new Date().getFullYear()} ValhallaDevelopment`,
+        icon: trayIcon,
+    };
+
+    await dialog.showMessageBox(options);
 }
